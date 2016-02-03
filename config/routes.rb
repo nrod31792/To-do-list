f764f9cf4c7a917219de674b4afcc019b3f27299
Nicholas Rodriguez <nrodriguez3179@gmail.com>
2016-02-03 15:29:18 -0500

    route name error fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'todo_items3welcome#index'
+  root 'todo_items#index'
 
   resources :todo_items
   # The priority is based upon order of creation: first created -> highest priority.
